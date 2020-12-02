528b14a47c7a60ae7a1e800542cf425991ad1637
premnath <premdevapp@gmail.com>
2020-12-02 12:19:36 +0530

    A bat animation fade in anim++

--- app/src/main/java/com/example/myanimationframe/MainActivity.java
+++ app/src/main/java/com/example/myanimationframe/MainActivity.java
@@ -6,37 +6,43 @@ import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private AnimationDrawable batAnimation;
-    private ImageView imageView;
+    private ImageView batImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView = findViewById(R.id.imageView);
+        batImage = (ImageView) findViewById(R.id.imageView);
 
-        imageView.setBackgroundResource(R.drawable.bat_anim);
+        //batImage.setBackgroundResource(R.drawable.bat_anim);
 
-        batAnimation = (AnimationDrawable) imageView.getBackground();
+        //batAnimation = (AnimationDrawable) batImage.getBackground();
 
 
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        batAnimation.start();
+        //batAnimation.start();
 
         Handler mhandler = new Handler();
         mhandler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                batAnimation.stop();
+
+                Animation startAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fadein_animation);
+                batImage.startAnimation(startAnimation);
+
+                //batAnimation.stop();
             }
-        }, 5000);
+        }, 50);
 
         return super.onTouchEvent(event);
     }
